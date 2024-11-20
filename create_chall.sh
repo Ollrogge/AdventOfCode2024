e3ab54f1b06299a60785244aaa3fd0aa8935a439
Ollrogge <nils-ollrogge@outlook.de>
2024-11-20 14:14:22 +0100

    Add script to simplify challenge creation

---
+++ create_chall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Please pass challenge name"
+    exit 0
+fi
+
+if [[ -e $1 ]]; then
+    echo "Directory already exists"
+    exit 0
+fi
+
+echo "Todo: Add script to automatically download input.txt"
+
+cargo new --bin $1
+
+code=$(cat <<END
+fn part1(input: &str) {
+
+}
+
+fn part2(input: &str) {
+
+}
+
+fn main() {
+  let input = include_str!("../input.txt");
+
+  part1(input);
+  part2(input);
+}
+END
+)
+
+echo "$code" > ./$1/src/main.rs
+
+
