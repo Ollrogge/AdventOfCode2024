6bf1333080c2bb116065d129524c3ce48f9d1dea
Ollrogge <nils-ollrogge@outlook.de>
2024-12-19 19:31:59 +0100

    day19 finish

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,21 +1,47 @@
+use std::collections::HashSet;
 use std::vec::Vec;
 
-fn parse_input(input: &str) -> (Vec<&str>, Vec<Vec<&str>>) {
+fn parse_input(input: &str) -> (Vec<&str>, HashSet<&str>) {
     let s = input.split("\n\n").collect::<Vec<&str>>();
-    let patterns: Vec<&str> = s[0].split(",").map(|p| p.trim().to_string()).collect();
+    let patterns: HashSet<&str> = s[0].split(",").map(|p| p.trim()).collect();
 
     let flags = s[1].lines().collect();
 
-    (patterns, flags)
+    (flags, patterns)
 }
 
-fn part1(input: &str) {}
+fn solve(input: &str) {
+    let (flags, patterns) = parse_input(input);
 
-fn part2(input: &str) {}
+    let mut possibilities: u64 = 0;
+    let mut possible: u64 = 0x0;
+
+    for flag in flags {
+        let n = flag.len();
+        let mut dp = vec![0u64; n + 1];
+        dp[0] = 1;
+
+        for i in 1..(n + 1) {
+            for pattern in patterns.iter() {
+                let l = pattern.len();
+                if i >= l && &flag[i - l..i] == *pattern {
+                    dp[i] += dp[i - l];
+                }
+            }
+        }
+
+        possibilities += dp[n];
+        if dp[n] > 0 {
+            possible += 1;
+        }
+    }
+
+    println!("{}", possible);
+    println!("{}", possibilities);
+}
 
 fn main() {
     let input = include_str!("../input.txt");
 
-    part1(input);
-    part2(input);
+    solve(input);
 }
